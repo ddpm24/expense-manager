3670aa19d3b7a8cd8c935a6d96bdde7a8823de6c
Deivi <deivi54357326@gmail.com>
2024-09-18 22:03:57 +0200

    Configuring the expense-entry component

---
+++ src/app/expense-entry/expense-entry.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { ExpenseEntry } from './expense-entry'; // Importación de la interfaz
+
+@Component({
+  selector: 'app-expense-entry',
+  templateUrl: './expense-entry.component.html',
+  styleUrls: ['./expense-entry.component.css']
+})
+export class ExpenseEntryComponent implements OnInit {
+  title: string = ''; // Inicializado con un string vacío
+expenseEntries: ExpenseEntry[] = []; // Inicializado con un array vacío
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.title = "Expense Entry List";
+    this.expenseEntries = this.getExpenseEntries(); // Inicializa la lista de gastos
+  }
+
+  getExpenseEntries() : ExpenseEntry[] { 
+    let mockExpenseEntries : ExpenseEntry[] = [ 
+      { id: 1, 
+         item: "Pizza", 
+         amount: Math.floor((Math.random() * 10) + 1), 
+         category: "Food", 
+         location: "Mcdonald", 
+         spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+         createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+      { id: 1, 
+         item: "Pizza", 
+         amount: Math.floor((Math.random() * 10) + 1), 
+         category: "Food", 
+         location: "KFC", 
+         spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+         createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+      { id: 1,
+        item: "Pizza",
+        amount: Math.floor((Math.random() * 10) + 1), 
+        category: "Food", 
+        location: "Mcdonald", 
+        spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+        createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+      { id: 1, 
+         item: "Pizza", 
+         amount: Math.floor((Math.random() * 10) + 1), 
+         category: "Food", 
+         location: "KFC", 
+         spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+         createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+      { id: 1, 
+         item: "Pizza", 
+         amount: Math.floor((Math.random() * 10) + 1), 
+         category: "Food", 
+         location: "KFC", 
+         spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+         createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) 
+      }, 
+    ]   
+    return mockExpenseEntries; 
+  }
+}
